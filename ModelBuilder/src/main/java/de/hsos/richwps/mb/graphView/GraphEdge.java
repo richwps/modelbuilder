3d00576cf1331b6486c25058b2deb93dc3d3d0c8
dziegenh <dennis.ziegenhagen@hs-osnabrueck.de>
2014-05-21 13:11:00 +0200

    Added custom edge model.

---
+++ ModelBuilder/src/main/java/de/hsos/richwps/mb/graphView/GraphEdge.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.hsos.richwps.mb.graphView;
+
+import com.mxgraph.model.mxCell;
+import com.mxgraph.model.mxGeometry;
+
+/**
+ *
+ * @author dziegenh
+ */
+public class GraphEdge extends mxCell {
+
+    private mxCell sourcePortCell;
+    private mxCell targetPortCell;
+
+    GraphEdge(Object value, mxGeometry mxGeometry, String style) {
+        super(value, mxGeometry, style);
+    }
+
+    public mxCell getSourcePortCell() {
+        return sourcePortCell;
+    }
+
+    public mxCell getTargetPortCell() {
+        return targetPortCell;
+    }
+
+    public void setSourcePortCell(mxCell sourcePortCell) {
+        this.sourcePortCell = sourcePortCell;
+    }
+
+    public void setTargetPortCell(mxCell targetPortCell) {
+        this.targetPortCell = targetPortCell;
+    }
+
+
+
+}
