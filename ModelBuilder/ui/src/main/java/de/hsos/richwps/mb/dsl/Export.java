698968177006d545523f7263e61c13c8021c7f58
Jewgeni Kovalev <jewgeni.kovalev@hs-osnabrueck.de>
2014-08-11 13:13:42 +0400

    Added topological sorting of processes, inputs and outputs for right order of execution

--- ModelBuilder/ui/src/main/java/de/hsos/richwps/mb/dsl/Export.java
+++ ModelBuilder/ui/src/main/java/de/hsos/richwps/mb/dsl/Export.java
@@ -1,6 +1,6 @@
 package de.hsos.richwps.mb.dsl;
 
-import com.mxgraph.model.mxCell;
+import com.mxgraph.model.mxICell;
 import de.hsos.richwps.dsl.api.Writer;
 import de.hsos.richwps.dsl.api.elements.Assignment;
 import de.hsos.richwps.dsl.api.elements.Execute;
@@ -9,12 +9,12 @@ import de.hsos.richwps.dsl.api.elements.OutReference;
 import de.hsos.richwps.dsl.api.elements.Reference;
 import de.hsos.richwps.dsl.api.elements.VarReference;
 import de.hsos.richwps.dsl.api.elements.Worksequence;
+import de.hsos.richwps.mb.Logger;
 import de.hsos.richwps.mb.graphView.mxGraph.Graph;
 import de.hsos.richwps.mb.graphView.mxGraph.GraphEdge;
 import de.hsos.richwps.mb.graphView.mxGraph.GraphModel;
 import de.hsos.richwps.mb.semanticProxy.entity.ProcessEntity;
 import de.hsos.richwps.mb.semanticProxy.entity.ProcessPort;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -32,9 +32,6 @@ public class Export {
      * output variables get their values from the reference map
      */
     protected Map<String, Reference> variables;
-    protected List<mxCell> processVertices;
-    protected List<mxCell> outputVertices;
-    protected List<mxCell> inputVertices;
     protected Graph graph;
 
     /**
@@ -44,138 +41,125 @@ public class Export {
     public Export(Graph graph) {
         this.graph = graph;
         this.variables = new HashMap<String, Reference>();
-        this.inputVertices = new ArrayList<mxCell>();
-        this.processVertices = new ArrayList<mxCell>();
-        this.outputVertices = new ArrayList<mxCell>();
-
-        this.initVertices();
     }
-
+    
     /**
-     * Iterates through all vertices, detects inputs, outputs and processes
-     */
-    protected void initVertices(){
-        System.out.println("Initializing vertices");
-        Object[] vertices = graph.getChildVertices(graph.getDefaultParent());
-        GraphModel model = graph.getGraphModel();
-        for (Object vertex : vertices) {
-            if (model.isProcess(vertex)) {
-                System.out.println("Process found");
-                this.processVertices.add((mxCell) vertex);
-            } else if (model.isFlowInput(vertex)) {
-                System.out.println("Output found");
-                this.outputVertices.add((mxCell) vertex);
-            } else if (model.isFlowOutput(vertex)) {
-                System.out.println("Input found");
-                this.inputVertices.add((mxCell) vertex);
-            }
-        }
-    }
-
-    /**
-     * Iterates through global inputs, assigns inputs to variables in the reference map
+     * Defines one global input, assigns inputs to variables in the
+     * reference map
+     *
      * @param ws Worksequence to write to
      * @throws Exception
      */
-    protected void defineInputs(Worksequence ws) throws Exception {
-        System.out.println("Defining input variables.");
-        for (mxCell input : this.inputVertices) {
-            ProcessPort source = (ProcessPort) input.getValue();
-            // @TODO: getOwsIdentifier is not ideal, collisions possible
-            String identifier = source.getOwsIdentifier();
-            VarReference variable = new VarReference(identifier);
-            InReference inputReference = new InReference(identifier);
-            Assignment assignment = new Assignment(variable, inputReference);
-            // Save variable to variable reference map
-            this.variables.put(identifier, variable);
-            // Write assignment to Worksequence
-            System.out.println("Assignment: "+assignment.toNotation());
+    protected void defineInput(mxICell input, Worksequence ws) throws Exception {
+        Logger.log("Defining input variable.");
+        ProcessPort source = (ProcessPort) input.getValue();
+        // @TODO: getOwsIdentifier is not ideal, collisions possible
+        String identifier = source.getOwsIdentifier();
+        VarReference variable = new VarReference(identifier);
+        InReference inputReference = new InReference(identifier);
+        Assignment assignment = new Assignment(variable, inputReference);
+        // Save variable to variable reference map
+        this.variables.put(identifier, variable);
+        // Write assignment to Worksequence
+        Logger.log("Assignment: " + assignment.toNotation());
+        ws.add(assignment);
+    }
+
+    /**
+     * Adds one process execute statement to worksequence, gets inputs from variables, stores
+     * outputs to variables
+     *
+     * @param processVertex
+     * @param ws Wroksequence to write to
+     * @throws Exception
+     */
+    protected void defineExecute(mxICell processVertex, Worksequence ws) throws Exception {
+        GraphModel model = graph.getGraphModel();
+        
+        // Get inputs from variable reference map
+        Object[] incoming = graph.getIncomingEdges(processVertex);
+        ProcessEntity process = (ProcessEntity) processVertex.getValue();
+        // @TODO: identifier not ideal, not in $org/name$ syntax
+        String identifier = process.getIdentifier();
+        Execute execute = new Execute(identifier);
+        for (Object in : incoming) {
+            GraphEdge edge = (GraphEdge) in;
+            ProcessPort source = (ProcessPort) edge.getSourcePortCell().getValue();
+            ProcessPort target = (ProcessPort) edge.getTargetPortCell().getValue();
+            // Reading varibale from varibale reference map
+            Reference variable = this.variables.get(source.getOwsIdentifier());
+            execute.addInput(variable, target.getOwsIdentifier());
+        }
+        
+        // Store outputs to variable refenrece map
+        Object[] outgoing = graph.getOutgoingEdges(processVertex);
+        for (Object out : outgoing) {
+            GraphEdge edge = (GraphEdge) out;
+            ProcessPort source = (ProcessPort) edge.getSourcePortCell().getValue();
+            // @TODO: identifier is not ideal
+            String outIdentifier = source.getOwsIdentifier();
+            // Writing varibale to varibale reference map
+            VarReference variable = new VarReference(outIdentifier);
+            // @TODO: set correct variable value
+            Reference asd = this.variables.put(outIdentifier, variable);
+            execute.addOutput(outIdentifier, variable);
+        }
+        Logger.log("Execute: " + execute.toNotation());
+        ws.add(execute);
+    }
+
+    /**
+     * Defines global output, gets its value from the reference map
+     *
+     * @param ws
+     * @throws Exception
+     */
+    protected void defineOutput(mxICell output, Worksequence ws) throws Exception {
+        Logger.log("Defining output variable.");
+        // Check incoming edges and look up the values in variables from the reference map
+        Object[] incoming = graph.getIncomingEdges(output);
+        for (Object in : incoming) {
+            GraphEdge edge = (GraphEdge) in;
+            ProcessPort source = (ProcessPort) edge.getSourcePortCell().getValue();
+            ProcessPort target = (ProcessPort) edge.getTargetPortCell().getValue();
+            // @TODO: identifier is not ideal
+            String inIdentifier = source.getOwsIdentifier();
+            String outIdentifier = target.getOwsIdentifier();
+            // Reading varibale from varibale reference map
+            Reference variable = this.variables.get(inIdentifier);
+            Reference outputReference = new OutReference(outIdentifier);
+            Assignment assignment = new Assignment(outputReference, variable);
+            Logger.log("Assignment: " + assignment.toNotation());
             ws.add(assignment);
         }
     }
 
     /**
-     * Iterates through Processeses, gets their inputs from variables, stores outputs to variables
-     * @param ws Wroksequence to write to
-     * @throws Exception
-     */
-    protected void defineExecutes(Worksequence ws) throws Exception {
-        // 2. iterate through processes
-        // read inputs from variable reference map,
-        // write outputs to variable reference map
-        GraphModel model = graph.getGraphModel();
-        for(mxCell vertex : this.processVertices){
-            // 2.1 get inputs from variable reference map
-            Object[] incoming = graph.getIncomingEdges(vertex);
-            ProcessEntity process = (ProcessEntity) vertex.getValue();
-            // @TODO: identifier not ideal, not in $org/name$ syntax
-            String identifier = process.getIdentifier();
-            Execute execute = new Execute(identifier);
-            for(Object in : incoming){
-                GraphEdge edge = (GraphEdge) in;
-                ProcessPort source = (ProcessPort) edge.getSourcePortCell().getValue();
-                ProcessPort target = (ProcessPort) edge.getTargetPortCell().getValue();
-                // Reading varibale from varibale reference map
-                Reference variable = this.variables.get(source.getOwsIdentifier());
-                execute.addInput(variable, target.getOwsIdentifier());
-            }
-            // 2.2 store outputs to variable refenrece map
-            Object[] outgoing = graph.getOutgoingEdges(vertex);
-            for(Object out : outgoing){
-                GraphEdge edge = (GraphEdge) out;
-                ProcessPort source = (ProcessPort) edge.getSourcePortCell().getValue();
-                // @TODO: identifier is not ideal
-                String outIdentifier = source.getOwsIdentifier();
-                // Writing varibale to varibale reference map
-                VarReference variable = new VarReference(outIdentifier);
-                // @TODO: set correct variable value
-                Reference asd = this.variables.put(outIdentifier, variable);
-                execute.addOutput(outIdentifier, variable);
-            }
-            System.out.println("Execute: "+execute.toNotation());
-            ws.add(execute);
-        }
-    }
-
-    /**
-     * Iterates through global outputs, gets their values from the reference map
-     * @param ws
-     * @throws Exception
-     */
-    protected void defineOutputs(Worksequence ws) throws Exception {
-        System.out.println("Defining output variables.");
-        for (mxCell output : this.outputVertices) {
-            // Check incoming edges and look up the values in variables from the reference map
-            Object[] incoming = graph.getIncomingEdges(output);
-            for(Object in : incoming){
-                GraphEdge edge = (GraphEdge) in;
-                ProcessPort source = (ProcessPort) edge.getSourcePortCell().getValue();
-                ProcessPort target = (ProcessPort) edge.getTargetPortCell().getValue();
-                // @TODO: identifier is not ideal
-                String inIdentifier = source.getOwsIdentifier();
-                String outIdentifier = target.getOwsIdentifier();
-                // Reading varibale from varibale reference map
-                Reference variable = this.variables.get(inIdentifier);
-                Reference outputReference = new OutReference(outIdentifier);
-                Assignment assignment = new Assignment(outputReference, variable);
-                System.out.println("Assignment: "+assignment.toNotation());
-                ws.add(assignment);
-            }
-        }
-    }
-
-    /**
      * Launches the exporting process and writes results to specifed file
+     *
      * @param path file to write to
      * @throws Exception
      */
     public void export(String path) throws Exception {
         Writer writer = new Writer();
         Worksequence ws = new Worksequence();
-        defineInputs(ws);
-        defineExecutes(ws);
-        defineOutputs(ws);
-        System.out.println(ws.toNotation());
+        // Topological sort is used to resolve dependencies
+        TopologicalSorter sorter = new TopologicalSorter();
+        List<mxICell> sorted = sorter.sort(graph);
+        Logger.log("Sorting graph");
+        for (mxICell cell : sorted) {
+            if(this.graph.getGraphModel().isProcess(cell)){
+                this.defineExecute(cell, ws);
+            }
+            else if(this.graph.getGraphModel().isFlowInput(cell)){
+                this.defineOutput(cell, ws);
+            }
+            else if(this.graph.getGraphModel().isFlowOutput(cell)){
+                this.defineInput(cell, ws);
+            }
+        }
+        Logger.log(ws.toNotation());
         writer.create(path, ws);
     }
+
 }
---
+++ ModelBuilder/ui/src/main/java/de/hsos/richwps/mb/dsl/TopologicalSorter.java
@@ -0,0 +1,116 @@
+package de.hsos.richwps.mb.dsl;
+
+import com.mxgraph.model.mxCell;
+import com.mxgraph.model.mxICell;
+import de.hsos.richwps.mb.Logger;
+import de.hsos.richwps.mb.graphView.mxGraph.Graph;
+import de.hsos.richwps.mb.graphView.mxGraph.GraphEdge;
+import de.hsos.richwps.mb.semanticProxy.entity.ProcessEntity;
+import de.hsos.richwps.mb.semanticProxy.entity.ProcessPort;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class TopologicalSorter {
+
+    /**
+     * unresolved dependencies, used for topological sorting
+     */
+    protected Map<mxICell, List<mxICell>> unresolvedDependencies;
+    /**
+     * Set of processes with resolved dependencies for topological sorting
+     */
+    protected Set<mxICell> verticesWithoutDependencies;
+    /**
+     * Topologically sorted list of processes
+     */
+    protected List<mxICell> sorted;
+
+    /**
+     * List of all process Vertices
+     */
+    public TopologicalSorter() {
+        this.init();
+    }
+
+    private void init() {
+        this.unresolvedDependencies = new HashMap<mxICell, List<mxICell>>();
+        this.verticesWithoutDependencies = new HashSet<mxICell>();
+        this.sorted = new ArrayList<mxICell>();
+    }
+
+    /**
+     * Performs the topological sorting algorithm on the graph
+     *
+     * @param graph the graph to be sorted
+     * @return sorted vertices
+     */
+    public List<mxICell> sort(Graph graph) {
+        this.init();
+        Object[] vertices = graph.getChildVertices(graph.getDefaultParent());
+        for (Object vertex : vertices) {
+            // create list of unresolved dependencies for each vertix
+            if (!this.unresolvedDependencies.containsKey((mxCell) vertex)) {
+                this.unresolvedDependencies.put((mxCell) vertex, new ArrayList<mxICell>());
+            }
+            Object[] incoming = graph.getIncomingEdges(vertex);
+            // find vertices without dependencies
+            if (incoming.length == 0) {
+                this.verticesWithoutDependencies.add((mxCell) vertex);
+            } else {
+                List<mxICell> dependencies = this.unresolvedDependencies.get((mxCell) vertex);
+                for (Object edge : incoming) {
+                    mxCell dependency = (mxCell) ((GraphEdge) edge).getSource();
+                    dependencies.add(dependency);
+                }
+            }
+        }
+        // Topological Sort until every dependency is resolved
+        while (!this.verticesWithoutDependencies.isEmpty()) {
+            // Pick any vertix without dependencies and move to sorted list
+            mxICell vertex = this.verticesWithoutDependencies.iterator().next();
+            this.verticesWithoutDependencies.remove(vertex);
+            this.sorted.add(vertex);
+            Logger.log(getCellName(vertex) + " was resolved and added to sorted List.");
+            // Resolve dependencies on child vertices
+            Object[] outgoing = graph.getOutgoingEdges(vertex);
+            for (Object edge : outgoing) {
+                mxICell child = ((GraphEdge) edge).getTarget();
+                List<mxICell> dependencies = this.unresolvedDependencies.get(child);
+                dependencies.remove(vertex);
+                Logger.log("Outgoing connection to " + getCellName(child) + " resolved.");
+                // Queue child vertix if it has no further dependencies
+                if (dependencies.isEmpty()) {
+                    this.verticesWithoutDependencies.add(child);
+                    Logger.log(getCellName(child) + "has no further dependencies.");
+                }
+            }
+        }
+        
+        return sorted;
+    }
+
+    /**
+     * Gets a readable cell name for debugging
+     * @TODO delete this function after testing
+     * @param cell
+     * @return readable cell name
+     */
+    private String getCellName(mxICell cell) {
+        Object entity = cell.getValue();
+        switch (cell.getValue().getClass().getSimpleName()) {
+            case "ProcessEntity": {
+                return (((ProcessEntity) entity).getIdentifier());
+            }
+            case "ProcessPort": {
+                return (((ProcessPort) entity).getOwsIdentifier());
+            }
+            default: {
+                return cell.getValue().getClass().getName();
+            }
+        }
+    }
+}
