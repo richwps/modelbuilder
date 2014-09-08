2f775d866f3e78aceb779644c99352c336caaf2c
caduevel <carsten.duevel@hs-osnabrueck.de>
2014-09-08 21:38:05 +0200

    Created InputBoundingBoxDataSpecifier + Test. (Based on Input- Complex-/Literal-DataSpecifier)

---
+++ ModelBuilder/ui/src/main/java/de/hsos/richwps/mb/richWPS/entity/impl/specifier/InputBoundingBoxDataSpecifier.java
@@ -0,0 +1,241 @@
+package de.hsos.richwps.mb.richWPS.entity.impl.specifier;
+
+import de.hsos.richwps.mb.richWPS.entity.IInputSpecifier;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import net.opengis.wps.x100.CRSsType;
+import net.opengis.wps.x100.InputDescriptionType;
+import net.opengis.wps.x100.SupportedCRSsType;
+import org.n52.wps.client.transactional.InputDescriptionTypeBuilder;
+
+/**
+ * InputSpecifier for BoundingBoxData.
+ *
+ * @author caduevel
+ */
+public class InputBoundingBoxDataSpecifier implements IInputSpecifier {
+
+    private String identifier;
+    private String title;
+    private String theabstract;
+    private String defaultCRS;
+    private int minOccur = 0;
+    private int maxOccur = 0;
+
+    final private List<String> supportedCRS;
+
+    /**
+     * Constructs an empty InputSpecifier.
+     */
+    public InputBoundingBoxDataSpecifier() {
+        this.identifier = "";
+        this.title = "";
+        this.theabstract = "";
+        this.defaultCRS = "";
+        this.minOccur = 0;
+        this.maxOccur = 0;
+        this.supportedCRS = new ArrayList();
+    }
+
+    /**
+     * Initialize this specifier with a description containing BoundingBoxData.
+     *
+     * @param desc Inputdescription containing BoundingBoxData.
+     */
+    public InputBoundingBoxDataSpecifier(final InputDescriptionType desc) {
+        this.identifier = desc.getIdentifier().getStringValue();
+
+        if (desc.getAbstract() != null) {
+            this.theabstract = desc.getAbstract().getStringValue();
+        } else {
+            this.theabstract = "";
+        }
+
+        this.title = desc.getTitle().getStringValue();
+        SupportedCRSsType thetype = desc.getBoundingBoxData();
+
+        this.minOccur = desc.getMinOccurs().intValue();
+        this.maxOccur = desc.getMaxOccurs().intValue();
+
+        this.defaultCRS = thetype.getDefault().getCRS();
+        CRSsType supported = desc.getBoundingBoxData().getSupported();
+        String[] crsArray = supported.getCRSArray();
+
+        this.supportedCRS = new ArrayList();
+        this.supportedCRS.addAll(Arrays.asList(crsArray));
+    }
+
+    @Override
+    final public String getIdentifier() {
+        return identifier;
+    }
+
+    @Override
+    final public String getTitle() {
+        return title;
+    }
+
+    @Override
+    final public String getAbstract() {
+        return theabstract;
+    }
+
+    /**
+     *
+     * @return
+     */
+    final public String getDefaultvalue() {
+        return defaultCRS;
+    }
+
+    @Override
+    final public int getMinOccur() {
+        return minOccur;
+    }
+
+    @Override
+    final public int getMaxOccur() {
+        return maxOccur;
+    }
+
+    /**
+     *
+     * @param minOccur
+     */
+    final public void setMinOccur(int minOccur) {
+        this.minOccur = minOccur;
+    }
+
+    /**
+     *
+     * @param maxOccur
+     */
+    final public void setMaxOccur(int maxOccur) {
+        this.maxOccur = maxOccur;
+    }
+
+    /**
+     *
+     * @param identifier
+     */
+    final public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    /**
+     *
+     * @param title
+     */
+    final public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     *
+     * @param theabstract
+     */
+    final public void setAbstract(String theabstract) {
+        this.theabstract = theabstract;
+    }
+
+    /**
+     *
+     * @param defaultvalue
+     */
+    final public void setDefaultvalue(String defaultvalue) {
+        this.defaultCRS = defaultvalue;
+    }
+
+    public String getDefaultCRS() {
+        return defaultCRS;
+    }
+
+    public void setDefaultCRS(String defaultCRS) {
+        this.defaultCRS = defaultCRS;
+    }
+
+    /**
+     * Returns an InputDescription with the added BoundingBox Object.
+     *
+     * @return Initialized InputDescription
+     */
+    @Override
+    final public InputDescriptionType toInputDescription() {
+        InputDescriptionTypeBuilder desc;
+        desc = new InputDescriptionTypeBuilder(this.identifier, this.title,
+                BigInteger.valueOf(this.minOccur),
+                BigInteger.valueOf(this.maxOccur));
+        desc.setAbstract(this.theabstract);
+
+        if (supportedCRS.size() <= 0) {
+            desc.addNewBoundingBoxData(defaultCRS);
+
+        } else {
+            desc.addNewBoundingBoxData(defaultCRS, supportedCRS);
+        }
+        return desc.getIdt();
+    }
+
+    @Override
+    public String toString() {
+        return "InputBoundingBoxDataSpecifier{" + "identifier=" + identifier 
+                + ", title=" + title + ", theabstract=" + theabstract 
+                + ", defaultCRS=" + defaultCRS + ", minOccur=" + minOccur 
+                + ", maxOccur=" + maxOccur + ", supportedCRS=" 
+                + supportedCRS + '}';
+    }
+    
+    @Override
+    final public int hashCode() {
+        int hash = 7;
+        hash = 73 * hash + Objects.hashCode(this.identifier);
+        hash = 73 * hash + Objects.hashCode(this.title);
+        hash = 73 * hash + Objects.hashCode(this.theabstract);
+        hash = 73 * hash + Objects.hashCode(this.defaultCRS);
+        hash = 73 * hash + this.minOccur;
+        hash = 73 * hash + this.maxOccur;
+        hash = 73 * hash + Objects.hashCode(this.supportedCRS);
+        return hash;
+    }
+
+    /**
+     *
+     * @param obj
+     * @return
+     */
+    @Override
+    final public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final InputBoundingBoxDataSpecifier other = (InputBoundingBoxDataSpecifier) obj;
+
+        if (!Objects.equals(this.identifier, other.identifier)) {
+            return false;
+        }
+        if (!Objects.equals(this.title, other.title)) {
+            return false;
+        }
+        if (!Objects.equals(this.theabstract, other.theabstract)) {
+            return false;
+        }
+        if (this.minOccur != other.minOccur) {
+            return false;
+        }
+        if (this.maxOccur != other.maxOccur) {
+            return false;
+        }
+
+        if (!Objects.equals(this.defaultCRS, other.defaultCRS)) {
+            return false;
+        }
+
+        return Objects.equals(this.supportedCRS, other.supportedCRS);
+    }
+}
---
+++ ModelBuilder/ui/src/test/java/de/hsos/richwps/mb/richWPS/entity/impl/specifier/InputBoundingBoxDataSpecifierTest.java
@@ -0,0 +1,57 @@
+package de.hsos.richwps.mb.richWPS.entity.impl.specifier;
+
+import net.opengis.wps.x100.InputDescriptionType;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.n52.wps.client.transactional.InputDescriptionTypeBuilder;
+
+/**
+ *  BoundingBoxDataSpecifier Test.
+ *  @author caduevel
+ */
+public class InputBoundingBoxDataSpecifierTest {
+    
+    public InputBoundingBoxDataSpecifierTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getIdentifier method, of class InputBoundingBoxSpecifier.
+     */
+    @Test
+    public void testConstruction() {
+        System.out.println("testConstruction");
+        InputBoundingBoxDataSpecifier specifier;
+        specifier = new InputBoundingBoxDataSpecifier();
+        specifier.setIdentifier("aabb");
+        specifier.setTitle("aabb");
+        specifier.setAbstract("aabb's abstract");
+        specifier.setMinOccur(0);
+        specifier.setMaxOccur(1);
+        specifier.setDefaultvalue("aab");
+        
+        InputDescriptionType ogctype = specifier.toInputDescription();
+        InputBoundingBoxDataSpecifier specifier2;
+        specifier2 = new InputBoundingBoxDataSpecifier(ogctype);
+        assertEquals(specifier, specifier2);
+    }
+}
