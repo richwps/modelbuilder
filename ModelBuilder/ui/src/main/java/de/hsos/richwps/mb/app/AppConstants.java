a99c56089344f40891613ac4feb65168580e43b2
dziegenh <dennis.ziegenhagen@hs-osnabrueck.de>
2014-10-15 11:46:24 +0200

    Changed view length of process titles.

--- ModelBuilder/ui/src/main/java/de/hsos/richwps/mb/app/AppConstants.java
+++ ModelBuilder/ui/src/main/java/de/hsos/richwps/mb/app/AppConstants.java
@@ -129,7 +129,7 @@ public class AppConstants {
      */
     public final static String DEPLOY_DIALOG_TITLE = "Deploy process";
 
-    public static int PROCESS_TITLE_MAX_VIEW_LENGTH = 30;
+    public static int PROCESS_TITLE_MAX_VIEW_LENGTH = 50;
 
     final static String INPUT_PORT_COLOR_STRING = "bbe8c6";
     final static String OUTPUT_PORT_COLOR_STRING = "cfe1fc";
