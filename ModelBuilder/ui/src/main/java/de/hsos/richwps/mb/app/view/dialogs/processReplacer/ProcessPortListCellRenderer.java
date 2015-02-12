c0e8be109519fec452e07c1f6db0e6e93acd4ff2
dziegenh <dennis.ziegenhagen@hs-osnabrueck.de>
2015-02-12 17:16:46 +0100

    Implemented process replacement tool. Closes #70.

--- ModelBuilder/ui/src/main/java/de/hsos/richwps/mb/app/view/dialogs/processReplacer/ProcessPortListCellRenderer.java
+++ ModelBuilder/ui/src/main/java/de/hsos/richwps/mb/app/view/dialogs/processReplacer/ProcessPortListCellRenderer.java
@@ -1,7 +1,9 @@
 package de.hsos.richwps.mb.app.view.dialogs.processReplacer;
 
 import de.hsos.richwps.mb.entity.ProcessPort;
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Font;
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.JLabel;
 import javax.swing.JList;
@@ -16,7 +18,9 @@ public class ProcessPortListCellRenderer extends DefaultListCellRenderer {
     public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
         Component component = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
         if (null == value) {
-            ((JLabel) component).setText("(skip mapping)");
+            JLabel label = ((JLabel) component);
+            label.setText("(skip mapping)");
+            label.setFont(label.getFont().deriveFont(Font.ITALIC));
             
         } else {
             ((JLabel) component).setText(((ProcessPort) value).getOwsIdentifier());
