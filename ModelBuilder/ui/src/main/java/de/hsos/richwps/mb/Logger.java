df7fe94beb78f2fc583c6979ed77cc94b8e35348
dalcacer <d.alcacer@hs-osnabrueck.de>
2015-02-17 15:32:53 +0100

    Fixed logging. Removed double and triple logging.

--- ModelBuilder/ui/src/main/java/de/hsos/richwps/mb/Logger.java
+++ ModelBuilder/ui/src/main/java/de/hsos/richwps/mb/Logger.java
@@ -15,8 +15,6 @@ import org.apache.log4j.PatternLayout;
  */
 public class Logger {
 
-    private static final java.util.logging.Logger log = java.util.logging.Logger.getLogger(Logger.class.getName());
-    /*private static FileHandler handler;*/
     static org.apache.log4j.Logger log4j = org.apache.log4j.Logger.getLogger(Logger.class.getName());
 
     /**
@@ -28,12 +26,12 @@ public class Logger {
     public static void log(Object o) {
         String oString = (null == o) ? "null" : o.toString();
         /*String micro = (new Long(System.nanoTime())).toString();
-        System.err.println(micro.substring(0, 6) + ": " + oString*/
+         System.err.println(micro.substring(0, 6) + ": " + oString*/
 
         try {
             PatternLayout layout = new PatternLayout("%d %p (%t) [%c] - %m%n");
-            ConsoleAppender consoleAppender = new ConsoleAppender(layout);
-            log4j.addAppender(consoleAppender);
+            /*ConsoleAppender consoleAppender = new ConsoleAppender(layout);
+            log4j.addAppender(consoleAppender);*/
             FileAppender fileAppender = new FileAppender(layout, "logs/ModelBuilder.log", false);
             log4j.addAppender(fileAppender);
             // ALL | DEBUG | INFO | WARN | ERROR | FATAL | OFF:
@@ -44,36 +42,15 @@ public class Logger {
         log4j.info(oString);
         /*log.info(oString);*/
     }
-    
-    
-    /**
-     * Uses Object.toString() for output to System.err. Prepends time
-     * information (µs).
-     *
-     * @param source
-     * @param locator
-     * @param o
-     */
-    public static void log(Class source, String locator,Object o) {
-        String oString = (null == o) ? "null" : source.getSimpleName()+"::"+locator+"\t"+o.toString();
-        Logger.log(oString);
-        /*String micro = (new Long(System.nanoTime())).toString();*/
-        /*System.err.println(micro.substring(0, 6) + ": " + oString);
 
-        try {
-            PatternLayout layout = new PatternLayout("%d %p (%t) [%c] - %m%n");
-            ConsoleAppender consoleAppender = new ConsoleAppender(layout);
-            log4j.addAppender(consoleAppender);
-            FileAppender fileAppender = new FileAppender(layout, "logs/ModelBuilder.log", false);
-            log4j.addAppender(fileAppender);
-            // ALL | DEBUG | INFO | WARN | ERROR | FATAL | OFF:
-            log4j.setLevel(org.apache.log4j.Level.ALL);
-        } catch (Exception e) {
-        }
-        log4j.info(oString);
-        log.info(oString);
-        */
+    /**
+     *
+     * @param source. e.g. this.getClass()
+     * @param locator e.g. method()
+     * @param o the log message.
+     */
+    public static void log(Class source, String locator, Object o) {
+        String oString = (null == o) ? "null" : source.getSimpleName() + "::" + locator + "\t" + o.toString();
+        Logger.log(oString);
     }
-    
-    
 }
