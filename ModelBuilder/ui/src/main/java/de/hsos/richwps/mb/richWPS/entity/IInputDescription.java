3d7db385769e0fd8a04065483bfda3079dcdfe55
cduevel <c.duevel93+git@gmail.com>
2015-03-13 10:37:53 +0100

    Added JavaDoc to IInputDescription

--- ModelBuilder/ui/src/main/java/de/hsos/richwps/mb/richWPS/entity/IInputDescription.java
+++ ModelBuilder/ui/src/main/java/de/hsos/richwps/mb/richWPS/entity/IInputDescription.java
@@ -4,44 +4,49 @@ import java.io.Serializable;
 import net.opengis.wps.x100.InputDescriptionType;
 
 /**
- *
+ * 
  * @author dalcacer
  */
 public interface IInputDescription extends Serializable { 
     
     /**
-     *
-     * @return
+     * @return  Unambiguous identifier or name of a process, input, or output,
+     *          unique for this server.
      */
     public String getIdentifier();
     
     /**
-     *
-     * @return
+     * @return  AbstractText. Brief narrative description
+     *          of a process, input, or output, normally available for display
+     *          to a human.
      */
     public String getAbstract();
     
     /**
      *
-     * @return
+     * @return  Title of a process, input, or output, normally available for 
+     *          display to a human.
      */
     public String getTitle();
     
     /**
-     *
-     * @return
+     * @return  Minimum number of times that values for this parameter are
+     *          required.
      */
     public int getMinOccur();
     
     /**
-     *
-     * @return
+     * @return  maxOccurs Maximum number of times that this parameter may
+     *          be present.
      */
     public int getMaxOccur();
     
     /**
      *
-     * @return
+     * @return An XML InputDescriptionType
+     * @see <a href="http://www.opengis.net/wps/1.0.0">
+     *          http://www.opengis.net/wps/1.0.0
+     *      </a>.
      */
     public InputDescriptionType toInputDescription();
 }
